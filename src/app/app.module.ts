ad0809bfd2ea3f0fa1f27572f0bb59adda1fa51f
Noah Ruben <nruben5684@gmail.com>
2023-03-01 13:29:38 -0500

    Form updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { NewGuitarFormComponent } from './new-guitar-form/new-guitar-form.component';
 
 @NgModule({
@@ -33,7 +34,8 @@ import { NewGuitarFormComponent } from './new-guitar-form/new-guitar-form.compon
     MatButtonModule,
     MatSelectModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/new-guitar-form/new-guitar-form.component.ts
+++ src/app/new-guitar-form/new-guitar-form.component.ts
@@ -1,11 +1,49 @@
 import { Component, OnInit, EventEmitter, Output} from '@angular/core';
 import { GuitarService } from '../services/guitar.service';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
+import * as _moment from 'moment';
+// tslint:disable-next-line:no-duplicate-imports
+import { default as _rollupMoment, Moment } from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'YYYY',
+  },
+  display: {
+    dateInput: 'YYYY',
+    monthYearLabel: 'YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'YYYY',
+  },
+};
 
 @Component({
   selector: 'app-new-guitar-form',
   templateUrl: './new-guitar-form.component.html',
-  styleUrls: ['./new-guitar-form.component.css']
+  styleUrls: ['./new-guitar-form.component.css'],
+  providers: [
+    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
+    // application's root module. We provide it at the component level here, due to limitations of
+    // our example generation script.
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 export class NewGuitarFormComponent implements OnInit {
 
@@ -27,12 +65,14 @@ export class NewGuitarFormComponent implements OnInit {
   createForm() {
     for (let field of Object.keys(this.guitarSchema)) {
       if (!['_id', '__v'].includes(field)) {
-        this.guitarFormGroup.addControl(field, new FormControl(''))
+        this.guitarFormGroup.addControl(field, new FormControl())
       }
     }
+    this.guitarFormGroup.setControl('year', new FormControl(moment()))
   }
 
   onSubmit(): void {
+    this.guitarFormGroup.setControl('year', new FormControl(this.guitarFormGroup.value['year']._d.getFullYear()))
     console.log(this.guitarFormGroup.value)
     this.guitarService.addGuitar(this.guitarFormGroup.value).subscribe((response: any) => {
       if (response) {
@@ -41,5 +81,12 @@ export class NewGuitarFormComponent implements OnInit {
       this.guitarFormGroup.reset()
       this.formSubmitted.emit()
     })
+  }
+
+  chosenYearHandler(normalizedYear: Moment, dp: any) {
+    const ctrlValue = this.guitarFormGroup.value['year'];
+    ctrlValue.year(normalizedYear.year());
+    this.guitarFormGroup.get('year')?.setValue(ctrlValue)
+    dp.close();
   }
 }
